f75cf99db710a5bf7ab3eddc8f079e6763e2123a
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-09-01 17:56:00 -0500

    new op_functions

--- functions_opcode.c
+++ functions_opcode.c
@@ -78,3 +78,62 @@ void op_nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ *
+ */
+void op_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	(void) line_number;
+
+	for (; current; current = current->next)
+		printf("%d\n", current->n);
+}
+
+/**
+ *
+ */
+void op_add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	stack_t *current = *stack;
+	int count = 0;
+
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+
+	if (count < 2)
+	{
+		if (tmp == NULL || (*stack)->next == NULL)
+			add_error(line_number);
+	}
+
+	(tmp->next)->n = tmp->n + (tmp->next)->n;
+	_pop(stack, line_number);
+}
+
+/**
+ *
+ */
+void op_pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack && (*stack)->next)
+	{
+		*stack = (*stack)->next;
+		free((*stack)->prev);
+	}
+	else if (*stack)
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+	else
+	{
+		dprintf(2, "L%d: can't pop an empty stack", line_number);
+		error(EXIT_FAILURE);
+	}
+}
