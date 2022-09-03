7294c489d8290131d8275b29d5922a0d34d06d23
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-09-02 20:24:53 -0500

    jump of line

--- op_functions.c
+++ op_functions.c
@@ -64,7 +64,7 @@ void op_pop(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack)
 	{
-		dprintf(2, "L%d: can't pop an empty stack", line_number);
+		dprintf(2, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
